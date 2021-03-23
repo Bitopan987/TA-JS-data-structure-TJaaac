c0301fcf0ee426e1604c5a2178375491274acc5e
Bitopan Deka <bitopanxon1@gmail.com>
2021-03-22 22:58:06 -0700

    Completed Assignment Accessing and Assigning

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -9,28 +9,31 @@
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
 let age = 20;
-
+let character = {};
+character = { characterName: "Arya" };
+character["character-age"] = age;
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
-
+character["sur name"] = "Stark";
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
-
+character["title"] = "Lady of Winterfell";
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
+character["greet"] = () => alert(`I am BITOPAN and my title is DEKA`);
 // - Add a new property to the character object named `isFemale` and value will be `true`
-
+character["isFemale"] = true;
 // - Execute the function stored in the greet key of the character object
-
+character["greet"];
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
-
+character["greet"] = () => alert(`She is TULSI and her title is KUMARI`);
 // - Check by calling `greet` and see if the message changed or not.
-
+character["greet"];
 //  - Add a new property in the character object `isAdult`. The value will be true if age is greater than 18 or it will be false.
-
+character["isAdult"] = age > 18 ? true : false;
 // - Add a new method (function inside objects are called methods) named `changeIsAdult`. When called should be able to flip the value of `isAdult` from true to false and false to true.
 
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
-let keyName = 'playedBy';
+let keyName = "playedBy";
 
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
 
